7e7e9b5018288a552c84364467b6462f8363696b
fzj <fengzongjian@yeah.net>
2017-12-27 13:19:20 +0800

    发布方式二：构建war包，1.需要引入tomcat环境依赖；2.修改启动类
    修改启动类：继承SpringBootServletInitializer类，并复写configure方法。

--- src/main/java/com/imooc/HotDeployFromImoocApplication.java
+++ src/main/java/com/imooc/HotDeployFromImoocApplication.java
@@ -2,9 +2,17 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+// 1.启动类继承SpringBootServletInitializer
 @SpringBootApplication
-public class HotDeployFromImoocApplication {
+public class HotDeployFromImoocApplication extends SpringBootServletInitializer {
+	// 2.复写configure方法
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HotDeployFromImoocApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotDeployFromImoocApplication.class, args);
